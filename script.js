65947fc7b74b409a4b660af51c16191930a28bc0
Mukesh Prakash <104923097+intr0vert-55@users.noreply.github.com>
2023-07-25 12:15:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function cg(){
+    bike = document.querySelector("#ninja");
+    bike.src = "./ninja-300-candy-lime-green.png";
+}
+function lg(){
+    ike = document.querySelector("#ninja");
+    bike.src = "./ninja-300-lime-green.png";
+}
+function md(){
+    ike = document.querySelector("#ninja");
+    bike.src = "./ninja-300-moondust-gray.png";
+}
+function cart(){
+    c = document.querySelector("#cart");
+    c.value = "Item added to cart";
+}
